11d4d56431fb6c4746786ab1c7719968a5d97510
unknown <packagingjanvi@gmail.com>
2025-11-08 13:45:07 +0530

    FIX: Router handler function crash

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
-// We will create this controller file in the next step.
 const campaignsController = require('../controllers/campaignsController');
 
 // --- Define Routes ---
@@ -14,10 +12,8 @@ router.get('/', campaignsController.getCampaignPage);
 // @desc    Start sending a new bulk message campaign
 router.post('/start', campaignsController.startCampaign);
 
-// --- ADD THIS NEW ROUTE ---
 // @route   POST /campaigns/test
 // @desc    Send a single test message for a campaign
 router.post('/test', campaignsController.sendTestMessage);
-// --- END OF NEW ROUTE ---
 
 module.exports = router;
